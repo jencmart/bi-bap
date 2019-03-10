8d77a6f9416552e60ec2ff46327e2473fab516eb
Your Name <martin.jenc@gmail.com>
2019-03-10 18:35:45 +0100

    all installs are working

--- src/algorithms-cpp/fast-lts/README.md
+++ src/algorithms-cpp/fast-lts/README.md
@@ -1,41 +1,47 @@
 # PIBYND 11 + cppimport + setuptools
 
-## Prerequisities
+## 1. Prerequisities
 * python
 * setuptools
 * pybind11
 * cppimport # it is not inside conda, need to install it through pip
-* eigen headers in ./src/lib/eigen
+* eigen headers in ./lib/eigen # ./lib/eigen/..all..the..files..
+* right now conda.recipe is unused...
 
+## 2. Development
+* edit files in ./src/fastlts.cpp
+* fastlts.cpp - c++ code with cppimport anotation
+* uses eigen library located in ./lib/eigen
+* ./tests/test-dev.py - uses cppimport to autocompile and import somecode.cpp
+* for cleanup purposes you can can use ./src/clean.sh
 
-## Development
-* edit files in ./src/devel/src
-* somecode.cpp - c++ code with cppimport anotation
-* uses eigen library located in ./src/lib/eigen
-* test.py - uses cppimport to autocompile and import somecode.cpp
-* for cleanup purposes you can can use run.sh
-* test-from-notebook - alternative to test.py, but it is better to use installed code
-
-* somefile.so (compiled code) will be created after cppimport compile the code, together with sum checksums (hidden files)
-    * feel free to delete it (test.py will complie it again)
+* fastlts.so (compiled code) will be created after cppimport compile the code, together with sum checksums (hidden files)
+    * feel free to delete it (./tests/test-dev.py will complie it again)
 
 ### TL`DR
-* python3  ./src/devel/src/test.py
-* or ./run.sh (does the same)
+* python3  ./tests/test-dev.py
 
+### Debug
+* all these three names needs to be the same say `xxx`
+	* `./src/xxx.cpp`
+		* inside this file `PYBIND11_MODULE(xxx, m)`
+	* `./tests/test-dev.py`
+		* `mujpackage = cppimport.imp("../src/xxx")`
 
-## Deployment
-* use ./setup.py
-* pip3 install ./
-* it'll use code from ./src/somecode.cpp
-* note that it is only simlink to ./src/devel/src/somecode.cpp
-* check if path to eigen library is correctly set up in ./setup.py
-	* expected path './src/lib/eigen'
+## 3. Deployment
+* to install module to the python environment use `./setup.py`
+* `pip3 install ./`
+* it'll use code from ./src/fastlts.cpp
+* don't worry about cppimort annotation - it's basically a comment
+* check if path to eigen library is correctly set up in `./setup.py`
+	* expected path `./lib/eigen`
 * when you install package in some environment, you can simply import it as package
-* see ./tests/test.py
+* see `./tests/test-setup.py`
 
-* if you edit the code in /.src/devel/src/somecode.cpp and want to reintall the packeage, copy it to ./src/somecode.cpp and remove cppimport header ?
+* you've edited the code in `/.src/fastlts.cpp` and want to reinstall the package? simply run `* pip3 install ./` again
 
 ### TL`DR
-* pip install ./setup.py
+* pip3 install ./
 
+### Debug
+* name of the module in `./setup.py` needs to be the same as in pybind annotation on `./src/fastlts.cpp` so check that if some import errors are happening
--- src/algorithms-cpp/fast-lts/tests/test-setup.py
+++ src/algorithms-cpp/fast-lts/tests/test-setup.py
@@ -1,5 +1,185 @@
-import python_example as m
+import cppimport
+import numpy as np
+import math
+import time
 
-assert m.__version__ == '0.0.1'
-assert m.add(1, 2) == 3
-assert m.subtract(1, 2) == -1
+from scipy import spatial
+
+import fastlts as eigen_lts
+
+class FastLtsEigenRegressor:
+    def __init__(self):
+        # public
+        self.n_iter_ = None
+        self.coef_ = None
+        self.intercept_ = None
+        self.h_subset_ = None
+        self.rss_ = None
+        self.time1_ = None
+        self.time2_ = None
+        self.time3_ = None
+        self.time_total_ = None
+
+    # currently support for np.ndarray and matrix
+    def _validate(self, X, y, h_size, num_start_c_steps, num_starts_to_finish, max_c_steps, threshold,
+                  use_intercept):
+        if X is None or not isinstance(X, (np.ndarray, np.matrix)):
+            raise Exception('X must be  type array or np.ndarray or np.matrix')
+        if y is None or not isinstance(y, (np.ndarray, np.matrix)):
+            raise Exception('y must be  type array or np.ndarray or np.matrix')
+
+        if X.ndim == 1:
+            X = np.reshape(X, [X.shape[0], 1])
+        if y.ndim == 1:
+            y = np.reshape(y, [y.shape[0], 1])
+
+        if type(X) is not np.ndarray:
+            X = np.ndarray(X)
+        if type(y) is not np.ndarray:
+            y = np.ndarray(y)
+
+        if y.ndim != 1:
+            if y.ndim != 2 or y.shape[1] != 1:
+                raise ValueError('y must be 1D array')
+        if y.shape[0] != X.shape[0]:
+            raise ValueError('X and y must have same number of samples')
+
+        if X.shape[0] < 1:  # expects N >= 1
+            raise ValueError('You must provide at least one sample')
+
+        if X.ndim < 1:  # expects p >=1
+            raise ValueError('X has zero dimensions')
+
+        if h_size != 'default':
+            if h_size > X.shape[0]:
+                raise ValueError('H_size must not be > number of samples')
+            if h_size < 1:
+                raise ValueError('H_size must be > 0 ; preferably (n + p + 1) / 2 <= h_size <= n ')
+
+        if max_c_steps < 1:  # expects max_steps => 1
+            raise ValueError('max_c_steps must be >= 1')
+
+        if num_start_c_steps < 1:  # expects num_start_steps => 1
+            raise ValueError('num_start_c_steps must be >= 1')
+
+        if num_starts_to_finish < 1:  # expects num_starts_to_finish >= 1
+            raise ValueError('num_starts_to_finish must be >= 1')
+
+        if threshold < 0:  # expects threshold >= 0
+            raise ValueError('threshold must be >= 0')
+
+        if use_intercept:
+            X = np.concatenate([X, np.ones((X.shape[0], 1))], axis=1)
+
+        return X, y
+
+
+    def fit(self, X, y,
+            num_starts: 'number of initial starts (H1)' = 500,
+            num_start_c_steps: 'number of initial C steps' = 2,
+            num_starts_to_finish: 'number of H3 which`ll to finish' = 10,
+            max_c_steps: 'self explanatory' = 50,
+            h_size: 'default := (n + p + 1) / 2' = 'default',
+            use_intercept=True,
+            threshold: 'stopping criterion Qold Qnew' = 1e-6):
+
+
+        X, y = self._validate(X, y, h_size, num_start_c_steps, num_starts_to_finish, max_c_steps, threshold,
+                              use_intercept)
+
+        # todo - include intercept or not? now - p include intercept..
+        _h_size = math.ceil((X.shape[0] + X.shape[1] +1) / 2) if h_size == 'default' else h_size  # N + p + 1
+
+        eigen_result = eigen_lts.fast_lts(X, y, num_starts, num_start_c_steps, num_starts_to_finish, _h_size,
+                                          max_c_steps, threshold)
+
+        # ... Store best result
+        weights = eigen_result.get_theta()
+        if use_intercept:
+            self.intercept_ = weights[-1, 0]  # last row first col
+            self.coef_ = np.ravel(weights[:-1, 0])  # for all but last column,  only first col
+        else:
+            self.intercept_ = 0.0
+            self.coef_ = np.ravel(weights[:, 0])  # all rows, only first col
+
+        self.h_subset_ = eigen_result.get_h_subset()
+        self.rss_ = eigen_result.get_rss()
+        self.n_iter_ = eigen_result.get_n_inter()
+        self.time1_ = eigen_result.get_time_1()
+        self.time2_ = eigen_result.get_time_2()
+        self.time3_ = eigen_result.get_time_3()
+        self.time_total_ = self.time1_ + self.time2_ + self.time3_
+
+
+###############################################
+
+# Data generator
+def generate_data_ND(cnt, dim, outlier_percentage=20, n_xij= (0,10), ei = (0,1), n_xi1_outlier = (100,10) ):
+    N_clean = cnt - int(math.floor(cnt / 100 * outlier_percentage))
+    N_dirty = int(math.ceil(cnt / 100 * outlier_percentage))
+
+    # Xij
+    mu, sigma = n_xij
+    X_clean = np.random.normal(mu,sigma, (N_clean, dim))
+    mu, sigma = n_xi1_outlier
+    X_outliers = np.random.normal(mu,sigma, (N_dirty, dim))
+
+    #ei
+    mu, sigma = ei
+    e = np.random.normal(mu, sigma, (N_clean, 1))
+    e_2 = np.random.normal(mu, sigma, (N_dirty, 1))
+
+    #Y
+    y_clean = np.concatenate((X_clean, e), axis=1)
+    y_clean = np.sum(y_clean, axis=1)
+
+    y_outliers = np.concatenate((X_outliers, e_2), axis=1)
+    y_outliers = np.sum(y_outliers, axis=1)
+
+    X = np.concatenate((X_clean, X_outliers), axis=0)
+    y = np.concatenate((y_clean, y_outliers), axis=0)
+
+
+    y = np.reshape(y, [y.shape[0], 1])
+    y_clean = np.reshape(y_clean, [y_clean.shape[0], 1])
+
+    return X, y, X_clean, y_clean
+
+
+######################################################
+######################################################
+if __name__ == '__main__':
+
+	assert eigen_lts.__version__ == '0.0.1'
+
+
+
+	X, y, X_clean, y_clean = generate_data_ND(1000, 5)
+
+	lts = FastLtsEigenRegressor()
+
+
+	# lts
+	lts.fit(X, y, use_intercept=True)
+	weights_correct = lts.coef_
+
+	# print data
+	print('rss: ', lts.rss_)
+	print('itr: ', lts.n_iter_)
+	print('tim: ', lts.time_total_)
+
+	#ols
+	lts.fit(X_clean, y_clean, use_intercept=True, h_size=X_clean.shape[0])
+	weights_expected = lts.coef_
+	# print('rsO: ', lts.rss_)
+	# cos similarity
+	result = 1 - spatial.distance.cosine(weights_correct, weights_expected)
+	print('cos: ', result)
+
+
+
+
+
+
+
+
